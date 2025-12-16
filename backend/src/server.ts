0f2478ebbe68ea917ae75544b7b1ee348f502498
Vato Mezvrishvili <jshecebe@gmail.com>
2025-12-16 17:49:39 +0400

    Fix CORS for new Netlify domain

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,9 +8,9 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://leverx-employee-service.netlify.app",
+      "https://courageous-froyo-f127c9.netlify.app",
     ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
