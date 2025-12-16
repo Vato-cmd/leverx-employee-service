df1e69383804d439f7643cdf5952614001bccfb5
Vato Mezvrishvili <jshecebe@gmail.com>
2025-12-16 17:56:30 +0400

    Sync latest frontend and backend changes

--- frontend/vite-env.d.ts
+++ frontend/vite-env.d.ts
@@ -1,1 +1,9 @@
-declare module "*.scss";
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
